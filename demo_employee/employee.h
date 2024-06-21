27aab6daf8ee67b97d542fffc84f7ae43466f680
zhangyemengren <624593758@qq.com>
2024-06-21 13:38:55 +0800

    feat: update

---
+++ demo_employee/employee.h
@@ -0,0 +1,43 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include <string>
+
+namespace Records {
+    const int DefaultRaiseAndDemeritAmount{1'000};
+
+    class Employee {
+    public:
+        Employee(const std::string& firstName,
+                 const std::string& lastName
+        );
+
+        void promote(int raiseAmount = DefaultRaiseAndDemeritAmount);
+        void demote(int demeritAmount = DefaultRaiseAndDemeritAmount);
+        void hire();
+        void fire();
+        void display() const;
+
+        void setFirstName(const std::string& firstName);
+        const std::string& getFirstName() const;
+
+        void setLastName(const std::string& lastName);
+        const std::string& getLastName() const;
+
+        void setEmployeeNumber(int employeeNumber);
+        int getEmployeeNumber() const;
+
+        void setSalary(int newSalary);
+        int getSalary() const;
+
+        bool isHired() const;
+
+    private:
+        std::string m_firstName;
+        std::string m_lastName;
+        int m_employeeNumber {-1};
+        int m_salary;
+        bool m_hired {false};
+    };
+}
+#endif //EMPLOYEE_H
